72791fb837bdd39adf6b09a1c45df64d9df3c467
jperry18999 <125463995+jperry18999@users.noreply.github.com>
2025-08-28 18:05:18 +0100

    logic for Top 10 Constituents by % Change

--- tests/scenario1-NavigateToSite.spec.ts
+++ tests/scenario1-NavigateToSite.spec.ts
@@ -15,13 +15,3 @@ test("Confirm user can Navigate to the London Stock Exchange website", async ({
     })
   ).toBeVisible();
 });
-
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-//});
